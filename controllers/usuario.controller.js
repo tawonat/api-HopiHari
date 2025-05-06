a56c8b10187bb9fb900f9af9a194c08ff974d5dd
tawonat <taloswi@gmail.com>
2025-05-06 11:29:06 -0300

    JWT arrumado

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -1,58 +1,77 @@
 const mysql = require('../mysql'); //biblioteca pro JS ler mysql 
 const bcrypt = require('bcrypt'); //biblioteca pro JS fazer hash da senha
+const jwt = require('jsonwebtoken'); //biblioteca pro JS fazer o token de autenticação 
+
+const JWT_SECRET = "SenhaMaluca"; //chave secreta do JWT, aqui só pra teste, depois tem que colocar no .env
 
 //rota pra POST cadastrar o usuário
 //aqui não precisa do id, pois o id é auto increment
 exports.cadastrarUsuario = async (req, res) => {
-    try{
+    try {
 
-const hashedPassword = await bcrypt.hash(req.body.password, 10); //faz o hash da senha
+        const hashedPassword = await bcrypt.hash(req.body.password, 10); //faz o hash da senha
 
         const resultado = await mysql.execute(
             `INSERT INTO users (first_name, last_name, email, password, birth_date) VALUES (?, ?, ?, ?, ?);`,
-            [req.body.first_name,req.body.last_name,req.body.email,hashedPassword,req.body.birth_date] //aqui já coloca a senha com hash
+            [req.body.first_name, req.body.last_name, req.body.email, hashedPassword, req.body.birth_date] //aqui já coloca a senha com hash
         );
-        return res.status(201).send({"Mensagem": "Usuário cadastrado com sucesso!", "Resultado": resultado});
+
+        //gerar token JWT
+        const token = jwt.sign({id: resultado.insertId, email}, JWT_SECRET, {expiresIn: "1h"}); //aqui gera o token com o id do usuário e a senha secreta, e define que o token vai expirar em 1 hora
+       
+        return res.status(201).send({ "Mensagem": "Usuário cadastrado com sucesso!", "Resultado": resultado });
     } catch (error) {
-        return res.status(500).send({"Mensagem": error});
+        return res.status(500).send({ "Mensagem": error });
     }
 }
 
 //rota de login
 exports.loginUsuario = async (req, res) => {
-    try{
+    try {
         const resultado = await mysql.execute(
             `SELECT * FROM users WHERE email = ?`,
             [req.body.email]
         );
-        if (resultado.length == 0){
-            return res.status(401).send({"Mensagem": "Email ou senha inválidos!"});
-        }
-        
-//função pra fazer alguma coisa do hash, não faço a minima ideia doq eu fiz aqui mas ta funcionando então nem compensa mexer
-        const senhacomparada = await bcrypt.compare(req.body.password, resultado[0].password); //compara a senha digitada com a senha do banco de dados
-        if (!senhacomparada){
-            return res.status(401).send({"Mensagem": "Email ou senha inválidos!"});
+        if (resultado.length == 0) {
+            return res.status(401).send({ "Mensagem": "Email ou senha inválidos!" });
         }
 
-        return res.status(201).send({"Mensagem": "Usuário logado com sucesso!", "Resultado": resultado});
+        //função pra fazer alguma coisa do hash, não faço a minima ideia doq eu fiz aqui mas ta funcionando então nem compensa mexer
+        const senhacomparada = await bcrypt.compare(req.body.password, resultado[0].password); //compara a senha digitada com a senha do banco de dados
+        if (!senhacomparada) {
+            return res.status(401).send({ "Mensagem": "Email ou senha inválidos!" });
+        }
+
+        //ROTA PRA CRIPTOGRAFIA JWT
+        const token = jwt.sign({
+            id: usuario[0].id,
+            first_name: resultado[0].first_name,
+            last_name: resultado[0].last_name,
+            email: resultado[0].email,
+            birth_date: resultado[0].birth_date
+        }, "senhadojwt")
+        return res.status(200).send({
+            "Mensagem": "Usuário logado com sucesso!",
+            "token": token,
+        });
     } catch (error) {
-        return res.status(500).send({"Mensagem": error});
+        return res.status(500).send({ "Mensagem": error });
     }
 }
 
+
 //rota pra PUT atualizar o usuário
 exports.atualizarUsuario = async (req, res) => {
-    try{
+    try {
         const idUsuario = Number(req.params.id);
 
         const resultado = await mysql.execute(
             `UPDATE users SET first_name = ?, last_name = ?, email = ?, password = ?, birth_date = ? WHERE id = ?;`,
-            [req.body.first_name,req.body.last_name,req.body.email,req.body.password,req.body.birth_date,idUsuario]
+            [req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.birth_date, idUsuario]
         );
-        return res.status(201).send({"Mensagem": "Usuário atualizado com sucesso!", "Resultado": resultado});
+        return res.status(201).send({ "Mensagem": "Usuário atualizado com sucesso!", "Resultado": resultado });
     } catch (error) {
-        return res.status(500).send({"Mensagem": error});
+        return res.status(500).send({ "Mensagem": error });
     }
 }
 
@@ -66,11 +85,11 @@ exports.deletarUsuario = async (req, res) => {
         );
 
         if (resultado.affectedRows === 0) {
-            return res.status(404).send({"Mensagem": "Usuário não encontrado!"});
+            return res.status(404).send({ "Mensagem": "Usuário não encontrado!" });
         }
 
-        return res.status(200).send({"Mensagem": "Usuário deletado com sucesso!", "Resultado": resultado});
+        return res.status(200).send({ "Mensagem": "Usuário deletado com sucesso!", "Resultado": resultado });
     } catch (error) {
-        return res.status(500).send({"Mensagem": error});
+        return res.status(500).send({ "Mensagem": error });
     }
 };
